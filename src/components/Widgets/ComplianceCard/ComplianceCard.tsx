92dd3c35b6880bb8871578a335e980062c6a6d9f
Marek Calus <marek-calus@wp.pl>
2020-12-14 22:52:42 +0100

    Add compliance card

---
+++ src/components/Widgets/ComplianceCard/ComplianceCard.tsx
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2020 RoadieHQ
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import React, { FC } from 'react';
+import { Link, List, ListItem } from '@material-ui/core';
+import Alert from '@material-ui/lab/Alert';
+import { InfoCard, Progress, StructuredMetadataTable } from '@backstage/core';
+import { Entity } from '@backstage/catalog-model';
+import { useProjectEntity } from '../../useProjectEntity';
+import {
+  useProtectedBranches,
+  useRepoLicence,
+} from '../../../hooks/useComplianceHooks';
+
+type ReleaseCardProps = {
+  entity: Entity;
+};
+
+const ComplianceCard: FC<ReleaseCardProps> = ({ entity }) => {
+  const { owner, repo } = useProjectEntity(entity);
+  const { branches, loading, error } = useProtectedBranches(entity);
+  const {
+    license,
+    loading: licenseLoading,
+    error: licenseError,
+  } = useRepoLicence(entity);
+
+  if (loading || licenseLoading) {
+    return <Progress />;
+  } else if (error || licenseError) {
+    return (
+      <Alert severity="error">
+        Error occured while fetching data for the compliance card:{' '}
+        {error?.message}
+      </Alert>
+    );
+  }
+
+  return (
+    <InfoCard title="Compliance report">
+      <StructuredMetadataTable
+        metadata={{
+          'Protected branches':
+            branches?.length && owner && repo ? (
+              <List>
+                {branches.map((branch: any) => (
+                  <ListItem key={branch.name}>{branch.name}</ListItem>
+                ))}
+              </List>
+            ) : (
+              <Alert severity="error">
+                You have no protected branches in the{' '}
+                {
+                  <Link href={`https:github.com/${owner}/${repo}`}>
+                    {owner}/{repo}
+                  </Link>
+                }{' '}
+                repository
+              </Alert>
+            ),
+          License:
+            license === 'No license file found' ? (
+              <Alert severity="error">
+                No license file found in the{' '}
+                {
+                  <Link href={`https:github.com/${owner}/${repo}`}>
+                    {owner}/{repo}
+                  </Link>
+                }{' '}
+                repository
+              </Alert>
+            ) : (
+              license
+            ),
+        }}
+      />
+    </InfoCard>
+  );
+};
+
+export default ComplianceCard;
