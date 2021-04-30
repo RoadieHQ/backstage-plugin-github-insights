69486f6c8e7f94e6b749f6bf3a0fc98d1ecbca6f
Irma12 <irma@roadie.io>
2021-04-30 14:13:56 +0200

    Added gif and webp extension

--- src/components/Widgets/ReadMeCard/ReadMeCard.tsx
+++ src/components/Widgets/ReadMeCard/ReadMeCard.tsx
@@ -114,7 +114,7 @@ const ReadMeCard = (props: Props) => {
             '[$1]' + `(//${hostname}/${owner}/${repo}/blob/${getRepositoryDefaultBranch(
               value.url
             )}/` + '$2$3)').replace(
-              /\[([^\[\]]*)\]\((?!https?:\/\/)(.*?)(\.png|\.jpg|\.jpeg)\)/gim,
+              /\[([^\[\]]*)\]\((?!https?:\/\/)(.*?)(\.png|\.jpg|\.jpeg|\.gif|\.webp)\)/gim,
               '[$1]' + `(//${hostname}/${owner}/${repo}/raw/${getRepositoryDefaultBranch(
                 value.url
               )}/` + '$2$3)')}
